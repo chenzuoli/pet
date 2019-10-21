e322d66ea2a19f855402281d9ec2e9e766ee3d27
chenzuoli <chenzuoli709@163.com>
2019-10-22 00:09:52 +0800

    spring boot visit jsp file.

--- src/main/java/top/wetech/pet/controller/UserController.java
+++ src/main/java/top/wetech/pet/controller/UserController.java
@@ -2,13 +2,23 @@ package top.wetech.pet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import top.wetech.pet.entity.User;
 import top.wetech.pet.service.UserService;
 
-@RestController
+/**
+ * 返回字符串
+ */
+// @RestController
+/**
+ * 返回的内容是去根目录寻找相应的jsp文件
+ */
+@Controller
 public class UserController {
     @Autowired
     JdbcTemplate jdbc;
@@ -29,6 +39,12 @@ public class UserController {
     @RequestMapping("/getUserByMobile")
     public User getUserByMobile(@RequestParam String mobile){
         return service.getUserByMobile(mobile);
+    }
+
+    @GetMapping("/getIndexJsp")
+    public String testJsp(Model model){
+        model.addAttribute("message", "This is index jsp page.");
+        return "index";
     }
 
 }
